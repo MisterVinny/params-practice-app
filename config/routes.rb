4bb44caba796e01c508dbb6cfeb7ae2b83046ca5
vinnyk <ragdolleye@gmail.com>
2021-05-09 14:34:52 -0500

    Adds route to use post method to pass the query_name body parameter.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # both routes assume variable passed as :query_name
   get "/query_caps" => "parameters#all_caps"
   get "/segment_caps/:query_name" => "parameters#all_caps"
+
+  # post request with same functionality as above
+  post "/post_caps" => "parameters#all_caps"
 end
